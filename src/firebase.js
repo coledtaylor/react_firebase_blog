c42312df3d538aae03b6458d0f0a46215769f86b
Cole Taylor <cole@fox-pest.com>
2021-03-08 15:36:15 -0700

    added env

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app'
 import "firebase/auth"
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyDyx6kxYpOcltskwdk8J7vbb6FTlY3Kkvs",
-  authDomain: "fox-blog-38a12.firebaseapp.com",
-  projectId: "fox-blog-38a12",
-  storageBucket: "fox-blog-38a12.appspot.com",
-  messagingSenderId: "134756566582",
-  appId: "1:134756566582:web:1f7af921acba50da433a97",
-  measurementId: "G-ML23SJT603"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId
 })
 
 export const auth = app.auth()
